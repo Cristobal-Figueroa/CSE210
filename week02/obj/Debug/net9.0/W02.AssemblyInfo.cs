b28ebe64eded5284445aa7e3f5171db6604daabd
Cristobal-Figueroa <cristobalfigueroa92@gmail.com>
2025-03-15 19:51:28 -0300

    more changes

--- week02/obj/Debug/net9.0/W02.AssemblyInfo.cs
+++ week02/obj/Debug/net9.0/W02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("W02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8354223a1304c4a0794212e04d114a473463677a")]
 [assembly: System.Reflection.AssemblyProductAttribute("W02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("W02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
