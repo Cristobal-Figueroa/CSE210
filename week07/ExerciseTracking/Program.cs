89b0423c0068eda5d58fc2a3036e6ff8a30535b0
Cristobal Figueroa <cristobalfigueroa92@gmail.com>
2025-04-17 13:13:57 -0400

    listp

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,30 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("Exercise Tracking Program");
+        Console.WriteLine("------------------------");
+
+        List<Activity> activities = new List<Activity>();
+
+        DateTime today = DateTime.Now;
+        
+        Running running = new Running(today, 30, 3.0);
+        activities.Add(running);
+        
+        Cycling cycling = new Cycling(today, 45, 15.0);
+        activities.Add(cycling);
+        
+        Swimming swimming = new Swimming(today, 60, 20);
+        activities.Add(swimming);
+
+        Console.WriteLine("\nActivity Summaries:");
+        foreach (Activity activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
